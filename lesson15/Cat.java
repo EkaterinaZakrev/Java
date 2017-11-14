6952644501e94466cc89c7b323709fd2ce10af11
EkaterinaZakrev <33605642+EkaterinaZakrev@users.noreply.github.com>
2017-11-14 19:48:04 +0200

    Add files via upload

---
+++ lesson15/Cat.java
@@ -0,0 +1,19 @@
+package by.home.lesson15;
+
+public class Cat {
+
+	String name = "Vasia";
+
+	public Cat() {
+	};
+
+	@Override
+	public String toString() {
+		return "Cat [name=" + name + "]";
+	}
+
+	public Cat(String name) {
+		this.name = name;
+	};
+
+}
