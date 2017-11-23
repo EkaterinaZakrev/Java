1fbfc1532d4bc06e4b5d284441b1e3f84b80b085
EkaterinaZakrev <33605642+EkaterinaZakrev@users.noreply.github.com>
2017-11-23 19:53:09 +0200

    Delete Convertion.java

--- Bank29102017/src/Convertion.java
+++
@@ -1,7 +0,0 @@
-public interface Convertion {
-    double fromCurrencytoUSD(double summ,Currency currency);
-    double fromUSDtoCurrency(double summ,Currency currency);
-    double fromCurrencytoCurrency(double summa,Currency from, Currency to);
-
-
-}
