b68e5a98d7d5909e44f15e585472cb908416fcf7
EkaterinaZakrev <33605642+EkaterinaZakrev@users.noreply.github.com>
2017-11-23 19:50:39 +0200

    Delete Convertion.java

--- Bank18112017/src/Convertion.java
+++
@@ -1,7 +0,0 @@
-public interface Convertion {
-    double fromCurrencytoBYN(double summ,Currency currency);
-    double fromBYNtoCurrency(double summ,Currency currency);
-    double fromCurrencytoCurrency(double summa,Currency from, Currency to);
-
-
-}
